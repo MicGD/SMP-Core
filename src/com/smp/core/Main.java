40a7b25de856fd8428fb05cbbd2bc49308e05e00
MicGD <55942477+MicGD@users.noreply.github.com>
2020-08-15 09:47:39 +0930

    Version 2.0
    Commands added, bug fixes, /fly command converted to ArrayList

--- src/com/smp/core/Main.java
+++ src/com/smp/core/Main.java
@@ -22,6 +22,8 @@ public class Main extends JavaPlugin {
         getCommand("teleport").setExecutor(new Commands());
         getCommand("broadcast").setExecutor(new Commands());
         getCommand("msg").setExecutor(new Commands());
+        getCommand("kill").setExecutor(new Commands());
+        getCommand("vault").setExecutor(new Commands());
         getServer().getPluginManager().registerEvents(new CreatureSpawn(), this);
         getServer().getPluginManager().registerEvents(new PlayerJoin(), this);
         getServer().getPluginManager().registerEvents(new PlayerLeave(), this);
--- src/com/smp/core/events/CreatureSpawn.java
+++ src/com/smp/core/events/CreatureSpawn.java
@@ -6,22 +6,19 @@ import org.bukkit.entity.*;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.CreatureSpawnEvent;
-import org.bukkit.event.player.PlayerInteractEvent;
-import org.bukkit.event.player.PlayerItemConsumeEvent;
-import org.bukkit.event.vehicle.VehicleMoveEvent;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.potion.Potion;
 import org.bukkit.potion.PotionEffect;
-import org.bukkit.potion.PotionEffect.*;
 import org.bukkit.potion.PotionEffectType;
-import org.bukkit.potion.PotionEffectType.*;
 
 public class CreatureSpawn implements Listener {
+
+
 
     @EventHandler
     public void creatureSpawn(CreatureSpawnEvent e) {
         if(e.getEntityType() == EntityType.CREEPER) {
             Creeper creeper = (Creeper)e.getEntity();
+            creeper.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, Integer.MAX_VALUE, 20));
             creeper.setPowered(true);
         }
         if(e.getEntityType() == EntityType.ZOMBIE) {
@@ -31,18 +28,16 @@ public class CreatureSpawn implements Listener {
             zombie.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 999, 20));
         }
         if(e.getEntityType() == EntityType.SKELETON) {
-            Skeleton opbow = (Skeleton)e.getEntity();
+            Skeleton skeleton = (Skeleton)e.getEntity();
             ItemStack bow = new ItemStack(Material.BOW);
             bow.addEnchantment(Enchantment.ARROW_DAMAGE, 5);
-            opbow.getEquipment().setItemInMainHand(bow);
+            skeleton.getEquipment().setItemInMainHand(bow);
         }
         if(e.getEntityType() == EntityType.SPIDER) {
             Spider spider = (Spider)e.getEntity();
             spider.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 999, 20));
         }
-        if(e.getEntityType() == EntityType.SHEEP){
-            Sheep sheep = (Sheep)e.getEntity();
-            sheep.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, Integer.MAX_VALUE, 30));
-        }
     }
+
 }
+
