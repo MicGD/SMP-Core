2c9c642369605c8fa1ac3d8eecf81950a2288512
MicGD <55942477+MicGD@users.noreply.github.com>
2020-08-15 09:48:47 +0930

    Version 2.0
    Bug fixes, commands added, /fly converted to ArrayList.

--- src/com/smp/core/Main.java
+++ src/com/smp/core/Main.java
@@ -5,6 +5,7 @@ import com.smp.core.events.CreatureSpawn;
 import com.smp.core.events.PlayerJoin;
 import com.smp.core.events.PlayerLeave;
 import com.smp.core.events.PlayerMove;
+import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
@@ -24,6 +25,7 @@ public class Main extends JavaPlugin {
         getCommand("msg").setExecutor(new Commands());
         getCommand("kill").setExecutor(new Commands());
         getCommand("vault").setExecutor(new Commands());
+        getServer().getConsoleSender().sendMessage(ChatColor.RED + "Plugin developed by Michele Pipicelli!");
         getServer().getPluginManager().registerEvents(new CreatureSpawn(), this);
         getServer().getPluginManager().registerEvents(new PlayerJoin(), this);
         getServer().getPluginManager().registerEvents(new PlayerLeave(), this);
